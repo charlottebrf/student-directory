a00a3c9b3c959b08733206db1a2b0ee9320197d7
Charlotte Fereday <cbrfereday@gmail.com>
2017-04-26 10:13:51 +0100

    Exercise 8:6: Adds a centers method to justify the output strings, including a note on refactoring

--- directory.rb
+++ directory.rb
@@ -1,11 +1,20 @@
+def centers(str)
+  puts str.center(150)
+end
+#Exercise 8:6 see note at the end on refactoring this method
+
+def asks_question(question)
+  centers("Please enter the #{question} of the students")
+end
+
 def input_students
   asks_question("names")
   asks_question("ages")
   asks_question("nationalities")
   asks_question("previous studies")
-  puts "To finish, just hit return five times"
-  #refactor the code so that you don't have to hit return 5 times & perhaps to have each question appear separately, rather than as a list at the top
-  
+  centers("To finish, just hit return five times")
+  #Exercise 8:5: refactor the code so that you don't have to hit return 5 times & perhaps to have each question appear separately, rather than as a list at the top
+
   #create an empty array
   students = []
   #get the first name
@@ -17,7 +26,7 @@ def input_students
   while !name.empty? && !age.empty? && !nationality.empty? && !studies.empty? do
     #add the student hash to the array
     students << {name: name, age: age, nationality: nationality, studies: studies, cohort: :november}
-    puts "Now we have #{students.count} students"
+    centers("Now we have #{students.count} students")
     name = gets.chomp
     age = gets.chomp
     nationality = gets.chomp
@@ -25,10 +34,6 @@ def input_students
   end
   #return the array of students
   students
-end
-
-def asks_question(question)
-  puts "Please enter the #{question} of the students"
 end
 
 
@@ -45,8 +50,8 @@ end
 
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  centers("The students of Villains Academy")
+  centers("-------------")
 end
 
 def print(students)
@@ -54,14 +59,16 @@ def print(students)
   index = 0
 
   while index < students.length do
-    puts "#{index + 1}. #{students[index][:name]}, #{students[index][:age]}, #{students[index][:nationality]}, #{students[index][:studies]} (#{students[index][:cohort]} cohort)"
+    centers("#{index + 1}. #{students[index][:name]}, #{students[index][:age]}, #{students[index][:nationality]}, #{students[index][:studies]} (#{students[index][:cohort]} cohort)")
     index += 1
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  centers("Overall, we have #{students.count} great students")
 end
+
+
 
 #nothing happens until we call the methods
 students = input_students
@@ -69,3 +76,19 @@ students = sorts_students(students)
 print_header
 print(students)
 print_footer(students)
+
+
+#Exercise 8:6
+# Refactor code to get the width of the terminal to use instead of a hard coded number, as I'm doing here. So as I/users resize terminal to a different width the text will still appear to be justified.
+# Require 'curses' didn't work
+
+# require 'curses'
+
+# def centers(str)
+#   puts str.center(Width)
+# end
+
+# screen = Curses.init_screen
+# Width = screen.maxx
+# height = screen.maxy
+# Curses.init_screen()
